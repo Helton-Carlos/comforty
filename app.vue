4dbaface6e9473fe29302c7f89259fa9ab6ded75
Helton Brito <heltoncarlospa@hotmail.com>
2024-06-22 19:06:50 -0300

    feat: default style tailwind base

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="bg-blue text-gray-dark px-4">
+    <h1 class="bg-primary text-gray px-4">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi voluptate
       fuga amet nostrum natus quis, voluptatum iusto accusantium quia impedit
       voluptatibus, corrupti dicta error ipsum animi nisi? Harum, vel veritatis.
