7d5b56ad8f2ce4119b6e3dc7778c4976e88bec89
Helton Brito <heltoncarlospa@hotmail.com>
2024-06-22 18:52:14 -0300

    feat: add tailwind css

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="bg-blue text-gray-dark px-4">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi voluptate
+      fuga amet nostrum natus quis, voluptatum iusto accusantium quia impedit
+      voluptatibus, corrupti dicta error ipsum animi nisi? Harum, vel veritatis.
+    </h1>
   </div>
 </template>
