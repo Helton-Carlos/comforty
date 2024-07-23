c5366dd4baa2a12145332eeeb2898c8cf02973a0
Helton Silva <helton.silva@melhorenvio.com>
2024-07-23 11:49:00 -0300

    feat: image slide

--- components/main-page.vue
+++ components/main-page.vue
@@ -1,10 +1,31 @@
 <script setup lang="ts">
 import banner1 from '~/assets/imagem/cadeira-1.png';
+import banner2 from '~/assets/imagem/poltrona-cama.png';
+import banner3 from '~/assets/imagem/poltrona.png';
 
 const chairList = [
   { path: banner1, name: 'cadeira retro' },
-  { path: banner1, name: 'cadeira retro2' },
+  { path: banner2, name: 'poltrona retro1' },
+  { path: banner3, name: 'poltrona retro2' },
 ];
+
+const image = ref<number>(0);
+
+function decrement(): void {
+if(image.value === 0) {
+    image.value = 3
+  }
+
+  image.value --
+}
+
+function increment(): void {
+  image.value ++;
+
+  if(image.value > 2) {
+    image.value = 0
+  }
+}
 </script>
 
 <template>
@@ -12,8 +33,11 @@ const chairList = [
     class="w-[90%] mx-auto p-4 py-8 bg-[url('assets/imagem/banner.png')] bg-no-repeat bg-center bg-cover lg:w-[60%]"
   >
     <div class="flex justify-between items-center px-2 py-8">
-      <div class="bg-white w-[48px] h-[48px] rounded-full text-white m-2">
-        <IconArrowLeft class="my-3.5 mx-3.5" />
+      <div 
+        class="hidden md:block bg-white w-[48px] h-[48px] rounded-full text-white m-2 hover:bg-primary" 
+        @click="decrement "
+      >
+        <IconArrowLeft class="my-3.5 mx-3"  />
       </div>
 
       <div class="pl-4 flex md:gap-10 md:mt-8">
@@ -21,42 +45,45 @@ const chairList = [
           <div class="my-8 text-center md:text-left">
             <p class="font-medium mb-2">Seja bem-vindo.</p>
             
-            <h3 class="text-xl font-bold tracking-tight leading-10 md:text-2xl">
+            <h3 class="text-xl font-bold tracking-tight leading-10 md:text-xl lg:text-2xl">
               Melhores móveis, Coleção para seu interior.
             </h3>
           </div>
 
-          <button
-            class="bg-primary mt-4 rounded-md p-2 px-4 md:mx-0 hover:bg-green"
+          <div
+            class="flex justify-center md:justify-start"
           >
             <nuxt-link 
               to="/product" 
-              class=" mx-auto flex text-white gap-2"
+              class="flex text-white gap-2 bg-primary mt-4 rounded-md p-2 px-4 shadow md:mx-0 hover:bg-green"
             >
               <span>Veja mais</span>
               <IconArrowNext />
             </nuxt-link>
-          </button>
+          </div>
         </div>
 
         <div>
           <img 
             class="hidden md:block" 
-            :src="chairList[0].path" 
-            :alt="chairList[0].name" 
+            :src="chairList[image].path" 
+            :alt="chairList[image].name" 
           />
         </div>
       </div>
 
-      <span class="bg-white w-[48px] h-[48px] rounded-full text-white m-2">
-        <IconArrowRight class="my-3.5 mx-3.5" />
+      <span 
+        class="hidden md:block bg-white w-[48px] h-[48px] rounded-full text-white m-2 shadow hover:bg-primary"
+        @click="increment " 
+      >
+        <IconArrowRight class="my-3.5 mx-3" />
       </span>
     </div>
 
-    <div class="flex justify-center gap-2">
-      <div class="bg-primary w-[8px] h-[8px] rounded-full"></div>
-      <div class="bg-gray-dark w-[8px] h-[8px] rounded-full"></div>
-      <div class="bg-gray-dark w-[8px] h-[8px] rounded-full"></div>
+    <div class="hidden md:flex md:justify-center md:gap-2">
+      <div :class="['bg-gray-dark w-[8px] h-[8px] rounded-full', { 'bg-primary' : image === 0 }]"></div>
+      <div :class="['bg-gray-dark w-[8px] h-[8px] rounded-full', { 'bg-primary' : image === 1 }]"></div>
+      <div :class="['bg-gray-dark w-[8px] h-[8px] rounded-full', { 'bg-primary' : image === 2 }]"></div>
     </div>
   </main>
 </template>
