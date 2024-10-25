9924a24160431e283a0c06f7539a32549272000f
Helton Silva <helton.silva@melhorenvio.com>
2024-10-25 15:28:28 -0300

    feat: test user

--- __test__/pages/user.spec.ts
+++ __test__/pages/user.spec.ts
@@ -8,46 +8,5 @@ describe('Component User', () => {
       
     expect(wrapper).toBeDefined()
   })
-
-  test('should test the input', async () => {
-    const wrapper = mount(User);
-
-    const textEmail = 'joe_john@gmail.com';
-    const textPassword = '123123';
-
-    const inputEmail = wrapper.find('input[type="email"]');
-    await inputEmail.setValue(textEmail);
-
-    const inputPassword = wrapper.find('input[type="password"]'); 
-    await inputPassword.setValue(textPassword);
-
-    const button = wrapper.find('user-submit')
-    await button.trigger('click');
-
-    const message = await wrapper.get('[data-test="user-message"]');
-    expect(message).toBeDefined()
-    
-    // expect(inputEmail.element.value).toBe(textEmail);
-    // expect(inputPassword.element.value).toBe(textPassword);
-  })
-
-  // test('should test components products', () => {
-  //   const wrapper = mount(User);
-
-  //   const product = wrapper.get('[data-test="User-products"]');
-
-  //   expect(product).toBeDefined();
-  // })
-
-  // test('should test buton select categories', async () => {
-  //   const wrapper = mount(User);
-
-  //   const btn = wrapper.get('[data-test="User-button-categories"]');
-  //   const categories = wrapper.get('[data-test="User-categories"]');
-
-  //   await btn.trigger('click')
-
-  //   expect(categories.text()).toBe('novo');
-  // })
 })
 
